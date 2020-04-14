de8b9ce199e449b05259c32abe83e7b8cc4786d6
george.charles <geroge.charles@oyohotels.cn>
2020-04-14 15:30:20 +0800

    feat: 添加webpack

--- index.js
+++
@@ -1,8 +0,0 @@
-
-
-function changeName() {
-    let element = document.getElementById('app')
-    element.innerText = "George"
-    console.log("daiyao")
-}
-
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var HtmlWebpackPluginEntry = new HtmlWebpackPlugin({
+  title: 'Claire',
+  template: path.join(__dirname, 'index.html'),
+})
+
+module.exports = {
+  // mode: 'production',
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/app.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[contenthash:6].[name].bundle.js',
+    // filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js?$/, loader: 'babel-loader' },
+      { test: /\.jsx?$/, loader: 'babel-loader' },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    historyApiFallback: true,
+  },
+  plugins: [HtmlWebpackPluginEntry],
+  resolve: {
+    alias: {
+      '@src': path.join(__dirname, 'src'),
+    },
+  },
+}
